a7658c960ff024abf3e484365169463cc1a89c94
Kirill Zlobin <jsdevzk@gmail.com>
2023-12-22 15:59:35 +0500

    can Sum - tabulated version

--- 34_canSumTab.js
+++ 34_canSumTab.js
@@ -1,3 +1,27 @@
 function canSum(numbers, target) {
-    
+
+    const table = Array(target + 1).fill(false)
+    table[0] = true
+
+    for (let i = 0; i < table.length; i++) {
+
+        if (table[i] === true) {
+            for (const num of numbers) {
+                if (i + num < table.length) {
+                    table[i + num] = true
+                }
+            }
+        }
+
+    }
+
+    return table[target]
+
 }
+
+console.log(canSum([7, 14], 300))
+console.log(canSum([7, 2], 9))
+
+//m -targetSum, n - array length
+//Time: O (m * n)
+//Space: O(table.length)
